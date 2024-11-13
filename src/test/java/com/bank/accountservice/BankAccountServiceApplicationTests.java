9251525341896ca6aa98e6676ed584be12e5bf8f
Oliver Castillo <ocastilt@nttdata.com>
2024-11-13 12:12:25 -0500

    chore: initial commit

---
+++ src/test/java/com/bank/accountservice/BankAccountServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.bank.accountservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankAccountServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Add tests
+	}
+
+}
